9844dd9b485d567c23eba4b47d52316493513930
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-04-10 16:11:42 +0545

    [add]: card

--- src/container.js
+++ src/container.js
@@ -1,33 +1,78 @@
-import Experience from './experience';
-
 import { CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer.js';
 
+// src
+import Experience from './experience';
+
+// utils
+import { status } from './utils/utils';
 export default class Container {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
 
-    this.setElement();
+    this.setCard();
   }
 
-  setElement() {
+  setCard() {
     this.element = document.createElement('div');
-    this.element.className = 'header-container';
+    this.element.className = 'card';
 
-    const header = document.createElement('p');
-    header.className = 'header';
-    header.innerHTML = 'HELLO WORLD';
-    this.element.appendChild(header);
+    // card-body
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    this.element.appendChild(cardBody);
 
-    console.log(this.element);
+    const cardAvatar = document.createElement('img');
+    cardAvatar.src = 'textures/avatar.svg';
+    cardAvatar.className = 'card-avatar';
+    cardBody.appendChild(cardAvatar);
+
+    const cardTitle = document.createElement('div');
+    cardTitle.className = 'card-title';
+    cardBody.appendChild(cardTitle);
+
+    const cardTitleFullname = document.createElement('p');
+    cardTitleFullname.className = 'card-title-fullname';
+    cardTitleFullname.innerHTML = 'Bibash Magar';
+    cardTitle.appendChild(cardTitleFullname);
+
+    const cardTitleUsername = document.createElement('p');
+    cardTitleUsername.className = 'card-title-username';
+    cardTitleUsername.innerHTML = '@bibash.mgr';
+    cardTitle.appendChild(cardTitleUsername);
+
+    // card-footer
+    const cardFooter = document.createElement('div');
+    cardFooter.className = 'card-footer';
+    this.element.appendChild(cardFooter);
+
+    for (let i = 0; i < 3; i++) {
+      this.createStatus(cardFooter, status[i].title, status[i].subtitle);
+    }
 
     const objectCSS = new CSS3DObject(this.element);
     objectCSS.position.x = 0;
     objectCSS.position.y = 0;
     objectCSS.position.z = 0;
 
-    console.log(objectCSS);
     this.scene.add(objectCSS);
+  }
+
+  createStatus(el, title, subtitle) {
+    let cardStatus = document.createElement('div');
+    cardStatus.className = 'card-status';
+
+    let cardStatusTitle = document.createElement('div');
+    cardStatusTitle.className = 'card-status-title';
+    cardStatusTitle.innerHTML = title;
+    cardStatus.appendChild(cardStatusTitle);
+
+    let cardStatusSubTitle = document.createElement('div');
+    cardStatusSubTitle.className = 'card-status-subtitle';
+    cardStatusSubTitle.innerHTML = subtitle;
+    cardStatus.appendChild(cardStatusSubTitle);
+
+    el.appendChild(cardStatus);
   }
 
   resize() {}
