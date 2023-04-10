13302cb0030b9666076bbc511e6f70370e128d99
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-04-10 16:11:21 +0545

    [add]: utils for status

---
+++ src/utils/utils.js
@@ -0,0 +1,14 @@
+export const status = [
+  {
+    title: 'Followers',
+    subtitle: '10K',
+  },
+  {
+    title: 'Likes',
+    subtitle: '99K',
+  },
+  {
+    title: 'Posts',
+    subtitle: '100',
+  },
+];
